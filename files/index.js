84bcdf767f1cb64b22e3f6156a5124acb68c86e5
Oscar Paredes <pago0812@gmail.com>
2016-12-08 17:21:53 -0600

    navidad

---
+++ files/index.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  //Array buttons
+  var buttons = Array.prototype.slice.call(document.getElementsByTagName('a'));
+  // Create a client instance
+  var hostname = "m13.cloudmqtt.com";
+  var port = 12837;
+  var username = "huauzaup"
+  var password = "7zO7RXczdyGa";
+  var path = "/navidad"
+  client = new Paho.MQTT.Client(hostname, port, path, "web_" + parseInt(Math.random() * 100, 10));
+
+  // connect
+  client.connect({
+    userName: username,
+    password: password,
+    onSuccess: function() {
+      console.log("onConnect");
+      buttons.forEach(function(button) {
+        client.subscribe(button.id);
+      })
+    },
+    onFailure: function(e) {
+      console.log("Fallo", e)
+    }
+  });
+
+  client.onConnectionLost = function(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  client.onMessageArrived = function(message) {
+    console.log("onMessageArrived:" + message.payloadString);
+    button = document.getElementById(message.destinationName);
+    if (button) {
+      if (message.payloadString == "off") {
+        button.classList.remove('active');
+      } else if (message.payloadString == "on") {
+        button.classList.add('active');
+      }
+    }
+  }
+
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      console.log(this.id)
+      if (this.classList.contains('active')) {
+        this.classList.remove('active');
+        message = new Paho.MQTT.Message("off");
+        message.destinationName = this.id;
+      } else {
+        this.classList.add('active');
+        message = new Paho.MQTT.Message("onn");
+        message.destinationName = this.id;
+      }
+      client.send(message);
+
+    })
+
+  });
+});
